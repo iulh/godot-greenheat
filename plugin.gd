6936ff3fd26ee8d1717d1cafa4de89545ab419ef
shaehl <z3z_a@hotmail.com>
2026-04-21 23:53:45 +0300

    fix: don't rely on hardcoded addon name `grren_heat`

--- plugin.gd
+++ plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	add_custom_type("GreenHeat", "Node", preload("res://addons/green_heat/green_heat.gd"), preload("res://addons/green_heat/green_heat.png"))
+	add_custom_type("GreenHeat", "Node", preload("green_heat.gd"), preload("green_heat.png"))
 
 
 func _exit_tree() -> void:
