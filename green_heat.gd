675a33101552b9091c187b7f063e7b933ffc17cf
shaehl <z3z_a@hotmail.com>
2026-04-22 11:15:09 +0300

    feat: locking `channel_name` and `enabled` variables, prettier debug messages

--- green_heat.gd
+++ green_heat.gd
@@ -8,17 +8,16 @@ signal input_received(input: GreenHeatInput) ## an exposed signal for detecting 
 		_debug_print("set enabled: %s" % value)
 		if value:
 			if is_node_ready():
-				connect_as("")
+				value = connect_as("")
 		else:
 			_disconnect_from_server()
 		_enabled = value
 	get():
 		return _enabled && !Engine.is_editor_hint()
+
 @export var channel_name : String = "": ## this is the channel name
 	set(value):
-		if (channel_name.length() > 0):
-			printerr("todo: switch channels on variable change")
-			return
+		if (channel_name.length() != 0 && enabled): return
 		channel_name = value
 
 var _debug = false
@@ -27,31 +26,35 @@ var _ws := WebSocketPeer.new()
 
 func _debug_print(text: String):
 	if _debug:
-		print(text)
+		print("[GH_%s] %s" % [get_instance_id(), text])
 
 func _ready():
 	if !enabled: return
 	_debug_print("connecting to GreenHeat on ready")
-	connect_as("")
+	_enabled = connect_as("")
 
 # connect to GreenHeat servers as the channel
 func connect_as(_channel_name: String):
 	if _channel_name.length() > 0:
 		channel_name = _channel_name
 
+	if channel_name.length() == 0:
+		printerr("can't connect to GreenHeat with an empty channel name")
+		return false
+
 	if _ws.get_ready_state() == WebSocketPeer.STATE_OPEN:
-		_debug_print("GreenHeat is connected already")
-		return
+		printerr("GreenHeat server is connected already")
+		return false
 
 	var url = _get_ws_url()
 	_debug_print("connecting to %s.." % url)
 	_ws.connect_to_url(url)
 
-	_enabled = true
+	return true
 	
 # connect to GreenHeat servers as the channel
 func _disconnect_from_server():
-	if _ws.get_ready_state() != _ws.STATE_OPEN:
+	if _ws.get_ready_state() != _ws.STATE_CLOSED:
 		var url = _get_ws_url()
 		_debug_print("hard disconnect from %s" % url)
 		_ws.close()
